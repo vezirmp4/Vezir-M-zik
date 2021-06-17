00589e0880a6e0e709bef03735e80ce1c516de07
vezirmp4 <86038464+vezirmp4@users.noreply.github.com>
2021-06-17 08:22:20 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js') // discord.js modülü tanımlıyoruz.
+const client = new Discord.Client() // client tanımalamsı
+const { readdirSync } = require('fs'); // tanımlamalar
+const { join } = require('path'); // tanımlamalar
+
+client.commands= new Discord.Collection(); // komutları alıyoruz
+
+const prefix = "!"
+
+const commandFiles = readdirSync(join(__dirname, "komutlar")).filter(file => file.endsWith(".js")); // Belli bir klasörden belli .js uzantılı dosyaları buluyor.
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "komutlar", `${file}`));
+    client.commands.set(command.kod, command); // Komutları Ayarlıyoruz.
+}
+
+client.on("error", console.error);
+
+
+client.on('ready', () => {
+    
+  
+    var actvs = [
+        `${prefix}yardım ${client.guilds.cache.size} sunucuyu`,
+        `${prefix}yardım ${client.users.cache.size} Kullanıcıyı`, 
+        `${prefix}yardım`
+    ];
+    
+    client.user.setActivity('gg/Ut9zkfFttW', { type: 'PLAYING' });
+    
+  
+      console.log ('_________________________________________');
+      console.log (`Kullanıcı İsmi     : ${client.user.username}`);
+      console.log (`Sunucular          : ${client.guilds.cache.size}`);
+      console.log (`Kullanıcılar       : ${client.users.cache.size}`);
+      console.log (`Prefix             : ?`);
+      console.log (`Durum              : Bot Çevrimiçi!`);
+      console.log ('_________________________________________');
+    
+  });
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.commands.has(command)) return message.channel.send(`Komut dosyamda **${command}** adlı bir komut bulamadım.`);
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+
+client.login('ODUyNzIzMTQzNTIzOTU4ODE0.YMK-UA.VIXQXkOYLPVrO7CujXPFWpdDQEE')
